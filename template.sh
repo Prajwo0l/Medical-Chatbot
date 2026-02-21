303883ab98913b9b2e3986bbe96b3a711420d856
Prajwol <lamichhaneprajwool@gmail.com>
2026-02-21 12:22:51 +0545

    Project Structure Added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research 
+
+
+touch src/__init__.py
+touch research/trials.ipynb
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+
+touch requirements.txt
+echo "Project and files created successfully!"
+
